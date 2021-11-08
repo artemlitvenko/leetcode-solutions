3362393c9969f528e8903862957080cf7b56c3b8
artemlitvenko <artem.sapiens@gmail.com>
2021-11-08 15:46:26 +0200

    add 1470_shuffle-the-array.js

---
+++ javascript/1470_shuffle-the-array.js
@@ -0,0 +1,15 @@
+/*
+    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+*/
+
+var shuffle = function(nums, n) {
+    let newArr = []
+    let arrX = nums.slice(0, n);
+    let arrY = nums.slice(n);
+    for (let i = 0; i < arrX.length; i++) {
+        newArr.push(arrX[i])
+        newArr.push(arrY[i])
+    }
+    return newArr;
+};
