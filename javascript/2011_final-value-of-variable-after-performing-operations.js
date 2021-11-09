bd044d58808015f355a18c04dd2cec93e5071f8a
artemlitvenko <artem.sapiens@gmail.com>
2021-11-09 10:13:58 +0200

    add 2011_final-value-of-variable-after-performing-operations.js

---
+++ javascript/2011_final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,32 @@
+/*
+    There is a programming language with only four operations and one variable X:
+
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+    Initially, the value of X is 0.
+
+    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+*/
+
+var finalValueAfterOperations = function(operations) {
+    let number = 0;
+    for (let i = 0; i < operations.length; i++) {
+        switch (operations[i]) {
+            case "X++":
+                number++;
+                break;
+            case "X--":
+                number--;
+                break;
+            case "++X":
+                ++number;
+                break;
+            case "--X":
+                --number;
+                break;
+            default:
+                number;
+        }
+    }
+    return number;
+};
