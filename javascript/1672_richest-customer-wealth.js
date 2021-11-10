ca1c050544b4d4e0f839f8024065cc1537046072
artemlitvenko <artem.sapiens@gmail.com>
2021-11-10 22:31:54 +0200

    add 1672_richest-customer-wealth.js

---
+++ javascript/1672_richest-customer-wealth.js
@@ -0,0 +1,13 @@
+/*
+
+*/
+
+var maximumWealth = function(accounts) {
+    let sum = 0;
+    let result = [];
+    for (let i = 0; i < accounts.length; i++) {
+        sum = accounts[i].reduce((a, b) => a + b);
+        result.push(sum);
+    }
+    return Math.max(...result);
+};
