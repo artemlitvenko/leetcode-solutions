a8c561fbf7d89aece93344d4daba5b1a05af267a
artemlitvenko <artem.sapiens@gmail.com>
2021-11-11 13:34:57 +0200

    add 709_to-lower-case.js

---
+++ javascript/709_to-lower-case.js
@@ -0,0 +1,7 @@
+/*
+    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+*/
+
+var toLowerCase = function(s) {
+    return console.log(s.toLowerCase())
+};
