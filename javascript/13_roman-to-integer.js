fc72dd39293f61738ada37c89daad14b4cf59f6f
artemlitvenko <artem.sapiens@gmail.com>
2021-11-17 09:47:08 +0200

    add 13_roman-to-integer.js

---
+++ javascript/13_roman-to-integer.js
@@ -0,0 +1,34 @@
+/*
+    Given a roman numeral, convert it to an integer.
+*/
+
+var romanToInt = function(s) {
+    const symbolList = {
+        I: 1,
+        IV: 4,
+        IX: 9,
+        V: 5,
+        X: 10,
+        XL: 40,
+        XC: 90,
+        L: 50,
+        C: 100,
+        CD: 400,
+        CM: 900,
+        D: 500,
+        M: 1000
+    }
+
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentNum = s[i];
+        const romePair = s[i + 1] ? currentNum + s[i + 1] : "";
+        if (symbolList[romePair]) {
+            result += symbolList[romePair];
+            i++;
+        } else {
+            result += symbolList[currentNum];
+        }
+    }
+    return result;
+};
