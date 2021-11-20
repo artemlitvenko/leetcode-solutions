94e3581f72908209b8d4f5a12eb8b78feb24e6a8
artemlitvenko <artem.sapiens@gmail.com>
2021-11-20 17:54:39 +0200

    add 1281_subtract-the-product-and-sum-of-digits-of-an-integer.js

---
+++ javascript/1281_subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,13 @@
+/*
+    Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+*/
+
+var subtractProductAndSum = function(n) {
+    let product = String(n).split('').reduce((total, value) => {
+        return total * +value
+    }, 1);
+    let sum = String(n).split('').reduce((total, value) => {
+        return total + +value
+    }, 0);
+    return product - sum;
+};
