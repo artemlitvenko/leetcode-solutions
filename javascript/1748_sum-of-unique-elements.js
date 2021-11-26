c43bc7f249d08376db406ce3093d762816c9ae28
artemlitvenko <artem.sapiens@gmail.com>
2021-11-26 16:46:12 +0200

    add 1748_sum-of-unique-elements.js

---
+++ javascript/1748_sum-of-unique-elements.js
@@ -0,0 +1,8 @@
+/*
+    You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+    Return the sum of all the unique elements of nums.
+*/
+
+var sumOfUnique = function(nums) {
+    return nums.filter((item, index, arr) => arr.lastIndexOf(item) === arr.indexOf(item)).reduce((sum, current) => sum += current, 0)
+};
