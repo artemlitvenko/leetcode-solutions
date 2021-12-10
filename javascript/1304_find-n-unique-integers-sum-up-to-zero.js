86e35b3225f03a969228ccc3c681e78b5f448117
artemlitvenko <artem.sapiens@gmail.com>
2021-12-10 09:18:04 +0200

    add 1304_find-n-unique-integers-sum-up-to-zero.js

---
+++ javascript/1304_find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,23 @@
+/*
+    Given an integer n, return any array containing n unique integers such that they add up to 0.
+*/
+
+var sumZero = function(n) {
+    const result = [];
+    const halfLength = n / 2;
+
+    if (n.length === 1) {
+        return [0];
+    }
+
+    if (n % 2) {
+        result.push(0);
+    }
+
+    for (let i = 1; i <= halfLength; i++) {
+        result.push(i);
+        result.push(-i);
+    }
+
+    return result;
+};
